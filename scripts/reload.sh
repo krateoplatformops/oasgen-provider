80d5b45c380a0d3d31cd96697bf8006bd5362304
Matteo Gastaldello <matteo.gastaldellomiotto@gmail.com>
2024-10-10 15:38:08 +0200

    feat: adds scripts for development

---
+++ scripts/reload.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Uninstall
+
+kubectl delete -f manifests/
+kubectl delete -f crds/
+
+# Install
+
+kubectl apply -f crds/
+kubectl apply -f manifests/
+
+
+# Deploy 
+# kubectl apply -f manifests/ns.yaml
+# kubectl apply -f manifests/sa.yaml
+# kubectl apply -f manifests/deployment.yaml
+# kubectl apply -f manifests/registration.yaml
+# kubectl apply -f manifests/service.yaml
